1a518f0660bebbcc29a947ef4ee1507896f3850a
marcinadd <marcinadd@gmail.com>
2020-12-14 15:50:22 +0100

    Create INSTRUCTION_PATTERN regexp

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,13 @@
+import re
+
 from src.warrior import Warrior
+
+INSTRUCTION_PATTERN = re.compile(r'^([A-Z]{3})'  # INSTRUCTION
+                                 r'(?:\.(AB|BA|[ABFXI]))?'  # MODIFIER
+                                 r'(?:\s+([#$*@{<}>])?([^\s,;]+))'  # A-MODE, A-VALUE
+                                 r'(?:\s*,\s*([#$*@{<}>])?([^\s]+)\s*)?'  # B-MODE, B-VALUE
+                                 r'(?:\s*;.*)?$'  # Non capturing optional comment
+                                 )
 
 
 def parse_warrior(file_handle):
